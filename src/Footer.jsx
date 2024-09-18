7f9f88701f9b6955764b92cce3e1804dd465c4ee
jdgmn <20140976@s.ubaguio.edu>
2024-09-18 16:00:04 +0800

    Delete src directory

--- src/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return(
-        <div>
-            <h5>APPDEV First Grading Exam</h5>
-            <h6>By: Jonah Andre De Guzman</h6>
-        </div>
-    )
-}
