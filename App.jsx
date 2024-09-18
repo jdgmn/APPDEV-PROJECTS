88a12251149f1c86f4da20f5139532fc51b720a3
jdgmn <20140976@s.ubaguio.edu>
2024-09-18 15:52:48 +0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header"
+import Overview from "./Overview"
+import Setup from "./Setup"
+import Footer from "./Footer"
+
+export default function App() {
+  return (
+    <>
+    <Header name="KB-X1000"/>
+    <Overview name="KB-X1000"/>
+    <Setup />
+    <Footer />
+    </>
+  )
+}
