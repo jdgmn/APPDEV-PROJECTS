0a0da0d39b1a50d21382990a1fc5415a7be11505
jdgmn <20140976@s.ubaguio.edu>
2024-11-13 16:19:37 +0800

    Add files via upload

---
+++ reactGroceryList-DeGuzman-JonahAndre/src/Footer.jsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+    return(
+        <>
+        <footer>
+            <center>
+                <hr />
+                <p>
+                    &copy; {new Date().getFullYear()} Grocery List || Written By: Jonah De Guzman
+                </p>
+            </center>
+
+        </footer>
+        </>
+    )
+}
---
+++ reactGroceryList-DeGuzman-JonahAndre/src/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return(
+        <>
+        <header>
+            <h1><center>Welcome to the De Guzman Grocery!</center></h1>
+            <hr></hr>
+        </header>
+        </>
+    )
+}
